2e1012b5324ce0232b9811d2941d63381665d30d
Teja Prafulla Karpuram <61840072+tejapraffulakarpuram007@users.noreply.github.com>
2021-11-14 16:16:55 +0930

    Add files via upload

---
+++ Polymorphic Asymetric Encryption/python keys.py
@@ -0,0 +1,82 @@
+# Generating a key
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend()
+    )
+public_key = private_key.public_key()
+
+# Storing the keys
+from cryptography.hazmat.primitives import serialization
+pem = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+with open('private_key.pem', 'wb') as f:
+    f.write(pem)
+
+pem = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+with open('public_key.pem', 'wb') as f:
+    f.write(pem)
+
+# Reading the keys back in (for testing )
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+with open("private_key.pem", "rb") as key_file:
+        private_key = serialization.load_pem_private_key(
+            key_file.read(),
+            password=None,
+            backend=default_backend()
+        )
+with open("public_key.pem", "rb") as key_file:
+        public_key = serialization.load_pem_public_key(
+            key_file.read(),
+            backend=default_backend()
+        )
+
+# Encrypting and decrypting
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+
+message = f = open('Teja Medical .txt', 'rb')
+message = f.read()
+f.close()
+
+
+encrypted = public_key.encrypt(
+        message,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+f = open('Teja Medical enc.txt', 'wb')
+f.write(encrypted)
+f.close()
+
+message = f = open('Teja Medical enc.txt', 'rb')
+message = f.read()
+f.close()
+
+original_message = private_key.decrypt(
+        encrypted,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+f = open('Teja Medical decr.txt', 'wb')
+f.write(original_message)
+f.close()
+
